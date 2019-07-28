bc041078e95bf6527d1dc548d5bb514c1b5562b2
Alex Neagu <hzl.alex@yahoo.com>
2019-07-29 00:16:14 +0300

    address_database_entity

---
+++ src/main/java/com/cegeka/academy/address/AddressTests.java
@@ -0,0 +1,12 @@
+package com.cegeka.academy.address;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(classes=Address.class)
+@Transactional
+public class AddressTests {
+
+
+}
