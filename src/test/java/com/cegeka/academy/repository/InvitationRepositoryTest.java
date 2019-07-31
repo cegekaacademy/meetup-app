8e74f3219aa4c151e288881f1637ab2646a3abc7
georgianabratucu <bratucu_georgiana@yahoo.com>
2019-07-31 11:39:11 +0300

    update invitation entity

--- src/test/java/com/cegeka/academy/repository/InvitationRepositoryTest.java
+++ src/test/java/com/cegeka/academy/repository/InvitationRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.cegeka.academy.repository;
 
 import com.cegeka.academy.AcademyProjectApp;
+import com.cegeka.academy.domain.Event;
 import com.cegeka.academy.domain.Invitation;
+import com.cegeka.academy.domain.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,17 +19,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class InvitationRepositoryTest {
 
     @Autowired InvitationRepository invitationRepository;
+    @Autowired EventRepository eventRepository;
+    @Autowired UserRepository userRepository;
 
     @Test
     public void testAddInvitation(){
         Invitation invitation = new Invitation();
         invitation.setStatus("pending");
         invitation.setDescription("ana are mere");
+        Event event = new Event();
+        event.setAddressId(1234L);
+        event.setDescription("Ana are mere!");
+        event.setName("KFC Krushers Party");
+        event.setPublic(true);
+        eventRepository.save(event);
+        invitation.setInvitationEvent(event);
+        User user = new User();
+        user.setLogin("login");
+        user.setPassword("anaanaanaanaanaanaanaanaanaanaanaanaanaanaanaanaanaanaanaana");
+        userRepository.save(user);
+        invitation.setInvitedUser(user);
         invitationRepository.save(invitation);
         List<Invitation> list = invitationRepository.findAll();
         assertThat(list.size()).isEqualTo(1);
         assertThat(list.get(0).getStatus()).isEqualTo(invitation.getStatus());
         assertThat(list.get(0).getDescription()).isEqualTo(invitation.getDescription());
+        assertThat(list.get(0).getInvitationEvent().getId()).isEqualTo(invitation.getInvitationEvent().getId());
+        assertThat(list.get(0).getInvitedUser().getId()).isEqualTo(invitation.getInvitedUser().getId());
 
     }
 
