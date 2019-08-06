896da76557efa819248e87a8bae7768cc050b31d
antonescu.adrian16@gmail.com <P@$$w0rd-1>
2019-08-06 20:13:17 +0300

    added service for events

--- src/main/java/com/cegeka/academy/service/event/EventServiceImpl.java
+++ src/main/java/com/cegeka/academy/service/event/EventServiceImpl.java
@@ -4,10 +4,12 @@ import com.cegeka.academy.domain.Event;
 import com.cegeka.academy.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class EventServiceImpl implements EventService {
 
     @Autowired
