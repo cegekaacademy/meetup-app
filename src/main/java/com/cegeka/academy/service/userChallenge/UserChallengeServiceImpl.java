0a1fec6808e98e7fa1a3e7a16caf679c7e94be37
iulianab <iuliana.bratucu.97@gmail.com>
2019-08-07 17:59:05 +0300

    create classes for UserChallenge

---
+++ src/main/java/com/cegeka/academy/service/userChallenge/UserChallengeServiceImpl.java
@@ -0,0 +1,9 @@
+package com.cegeka.academy.service.userChallenge;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserChallengeServiceImpl implements UserChallengeService {
+}
