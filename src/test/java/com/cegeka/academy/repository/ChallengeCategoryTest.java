d69a0a8cd58cf682df2fa3f54af0512734aa5e7c
iulianab <iuliana.bratucu.97@gmail.com>
2019-08-08 16:44:19 +0300

    modify ChallengeCategoryTest

--- src/test/java/com/cegeka/academy/repository/ChallengeCategoryTest.java
+++ src/test/java/com/cegeka/academy/repository/ChallengeCategoryTest.java
@@ -2,7 +2,7 @@ package com.cegeka.academy.repository;
 
 import com.cegeka.academy.AcademyProjectApp;
 import com.cegeka.academy.domain.ChallengeCategory;
-import com.cegeka.academy.domain.Interest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,12 +17,18 @@ class ChallengeCategoryTest {
     @Autowired
     private ChallengeCategoryRepository challengeCategoryRepository;
 
-    @Test
-    public void testAddChallengeCategory(){
-        ChallengeCategory challengeCategory =  new ChallengeCategory();
+    private ChallengeCategory challengeCategory;
+
+    @BeforeEach
+    public void init(){
+        challengeCategory =  new ChallengeCategory();
         challengeCategory.setName("category1");
         challengeCategory.setDescription("description1");
         challengeCategoryRepository.save(challengeCategory);
+    }
+
+    @Test
+    public void testAddChallengeCategory(){
         assertThat(challengeCategoryRepository.findAll().size()).isEqualTo(1);
         assertThat(challengeCategoryRepository.findAll().get(0).getName()).isEqualTo("category1");
         assertThat(challengeCategoryRepository.findAll().get(0).getDescription()).isEqualTo("description1");
@@ -30,19 +36,11 @@ class ChallengeCategoryTest {
 
     @Test
     public void testFindByName(){
-        ChallengeCategory challengeCategory =  new ChallengeCategory();
-        challengeCategory.setName("category1");
-        challengeCategory.setDescription("description1");
-        challengeCategoryRepository.save(challengeCategory);
         assertThat(challengeCategoryRepository.findByName("category1")).isEqualTo(challengeCategoryRepository.findAll().get(0));
     }
 
     @Test
     public void testFindByNameWithNoResult(){
-        ChallengeCategory challengeCategory =  new ChallengeCategory();
-        challengeCategory.setName("category1");
-        challengeCategory.setDescription("description1");
-        challengeCategoryRepository.save(challengeCategory);
         assertThat(challengeCategoryRepository.findByName("category2")).isEqualTo(null);
     }
 }
