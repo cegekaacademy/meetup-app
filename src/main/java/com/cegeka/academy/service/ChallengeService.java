7e3139a67aa247416631cd3734d302d53b3db0d3
Florin <florin.iutes@yahoo.com>
2019-08-08 17:23:13 +0300

    Created ChallengeService and ChallengeServiceImp

---
+++ src/main/java/com/cegeka/academy/service/ChallengeService.java
@@ -0,0 +1,5 @@
+package com.cegeka.academy.service;
+
+public interface ChallengeService {
+
+}
---
+++ src/main/java/com/cegeka/academy/service/ChallengeServiceImp.java
@@ -0,0 +1,11 @@
+package com.cegeka.academy.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class ChallengeServiceImp implements ChallengeService {
+
+}
