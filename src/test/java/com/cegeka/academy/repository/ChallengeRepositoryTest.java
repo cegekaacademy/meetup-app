b6d25aa682cb4679e9f8f5d0bb0553ca5acc10b4
iulianab <iuliana.bratucu.97@gmail.com>
2019-08-09 16:40:02 +0300

    modify ChallengeRepositoryTest

--- src/test/java/com/cegeka/academy/repository/ChallengeRepositoryTest.java
+++ src/test/java/com/cegeka/academy/repository/ChallengeRepositoryTest.java
@@ -5,6 +5,7 @@ import com.cegeka.academy.domain.Challenge;
 import com.cegeka.academy.domain.ChallengeCategory;
 import com.cegeka.academy.domain.User;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,8 +44,12 @@ public class ChallengeRepositoryTest {
 
     private User user;
 
+    private Challenge challenge;
+
+
     @BeforeEach
     public void init() {
+
         user = new User();
         user.setLogin(DEFAULT_LOGIN);
         user.setPassword(RandomStringUtils.random(60));
@@ -54,47 +59,37 @@ public class ChallengeRepositoryTest {
         user.setLastName(DEFAULT_LASTNAME);
         user.setImageUrl(DEFAULT_IMAGEURL);
         user.setLangKey(DEFAULT_LANGKEY);
-    }
-
-    @Test
-    public void canAddChallenge() {
-        Challenge challenge = new Challenge();
-        Date startDate = new Date();
-        Date endDate = new Date();
-
         User userTest = userRepository.save(user);
 
-        challenge.setCreator(userTest);
-        challenge.setPoints(5.22);
-        challenge.setStartDate(startDate);
-        challenge.setEndDate(endDate);
-        challenge.setStatus("new");
-
-        Challenge challengeTest = challengeRepository.save(challenge);
-
-        assertThat(challengeTest).isEqualTo(challenge);
-    }
-    @Test
-    public void canAddChallengeWithDescriptionAndCategory() {
-
-        Challenge challenge = new Challenge();
         Date startDate = new Date();
         Date endDate = new Date();
-
-        User userTest = userRepository.save(user);
 
         ChallengeCategory challengeCategory = new ChallengeCategory();
         challengeCategory.setName("challengeCategory");
         challengeCategory.setDescription("challengeCategoryDescription");
         challengeCategoryRepository.save(challengeCategory);
 
+        challenge = new Challenge();
         challenge.setCreator(userTest);
-        challenge.setPoints(22.0);
+        challenge.setPoints(5.22);
         challenge.setStartDate(startDate);
         challenge.setEndDate(endDate);
-        challenge.setStatus("status");
+        challenge.setStatus("new");
         challenge.setDescription("description");
         challenge.setChallengeCategory(challengeCategoryRepository.findAll().get(0));
+
+    }
+
+    @AfterEach
+    public void destroy(){
+
+        if(challenge != null) {
+            challengeRepository.delete(challenge);
+        }
+    }
+
+    @Test
+    public void canAddChallenge() {
 
         challengeRepository.save(challenge);
         Challenge challengeResult = challengeRepository.findAll().get(0);
