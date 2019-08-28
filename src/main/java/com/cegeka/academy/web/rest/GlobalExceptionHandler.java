36f2bfdbf25c5dd1432f1ae22e71924f7ec482d9
iulianab <iuliana.bratucu.97@gmail.com>
2019-08-28 18:23:29 +0300

    add handle for exceptions

--- src/main/java/com/cegeka/academy/web/rest/GlobalExceptionHandler.java
+++ src/main/java/com/cegeka/academy/web/rest/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.cegeka.academy.web.rest;
 
+import com.cegeka.academy.web.rest.errors.InvalidInvitationStatusException;
+import com.cegeka.academy.web.rest.errors.InvalidUserChallengeStatusException;
 import com.cegeka.academy.web.rest.errors.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,4 +31,20 @@ public class GlobalExceptionHandler{
 
         return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(value = InvalidInvitationStatusException.class)
+    public ResponseEntity<String> handleInvalidInvitationStatusException(InvalidInvitationStatusException exception) {
+
+        return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler(value = InvalidUserChallengeStatusException.class)
+    public ResponseEntity<String> handleInvalidUserChallengeStatusException(InvalidUserChallengeStatusException exception) {
+
+        return new ResponseEntity(exception.getMessage(), HttpStatus.BAD_REQUEST);
+
+    }
+
+
 }
