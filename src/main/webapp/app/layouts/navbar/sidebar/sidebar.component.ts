7f4203b44e73ce8b2e63327104efb7e4f7324175
Bianca Virtopeanu <bianca-georgiana.virtopeanu@ing.ro>
2019-08-28 17:18:32 +0300

    added sidebar component

--- src/main/webapp/app/layouts/navbar/sidebar/sidebar.component.ts
+++ src/main/webapp/app/layouts/navbar/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AccountService, LoginModalService, LoginService} from "app/core";
+import {Router} from "@angular/router";
+import {ProfileService} from "app/layouts/profiles/profile.service";
+import {NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: 'jhi-sidebar',
@@ -10,15 +14,40 @@ export class SidebarComponent implements OnInit {
     @Output() isMenuExpanded: EventEmitter<any> = new EventEmitter<any>();
 
     isExpanded = true;
+    inProduction: boolean;
+    swaggerEnabled: boolean;
+    modalRef: NgbModalRef;
+    version: string;
 
-    constructor() {
+
+    constructor(private loginService: LoginService,private accountService: AccountService,
+                private loginModalService: LoginModalService,
+                private profileService: ProfileService,
+                private router: Router) {
     }
 
     ngOnInit() {
+        this.profileService.getProfileInfo().then(profileInfo => {
+            this.inProduction = profileInfo.inProduction;
+            this.swaggerEnabled = profileInfo.swaggerEnabled;
+        });
     }
 
     toggleMenu() {
         this.isExpanded = !this.isExpanded;
         this.isMenuExpanded.emit({isExpanded: this.isExpanded});
+    }
+
+    isAuthenticated() {
+        return this.accountService.isAuthenticated();
+    }
+
+    login() {
+        this.modalRef = this.loginModalService.open();
+    }
+
+    logout() {
+        this.loginService.logout();
+        this.router.navigate(['']);
     }
 }
