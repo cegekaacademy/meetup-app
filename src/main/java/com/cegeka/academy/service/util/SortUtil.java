bd464fa7c654a48f9c5257ce75b467b9af7fa0e9
georgianabratucu <bratucu_georgiana@yahoo.com>
2019-08-30 12:34:23 +0300

    modified sort method

--- src/main/java/com/cegeka/academy/service/util/SortUtil.java
+++ src/main/java/com/cegeka/academy/service/util/SortUtil.java
@@ -1,26 +1,39 @@
 package com.cegeka.academy.service.util;
 
 import com.cegeka.academy.service.dto.UserDTO;
+import com.cegeka.academy.service.invitation.InvitationServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SortUtil {
 
+    private static Logger logger = LoggerFactory.getLogger(InvitationServiceImpl.class);
+
+
     public static List<UserDTO> sortUsersByDate(List<UserDTO> users) {
 
         return users.stream().sorted((o1, o2) -> {
-            if (o1.getCreatedDate() == null || o2.getCreatedDate() == null)
+
+            if (o1.getCreatedDate() == null && o2.getCreatedDate() == null) {
+
+                logger.info("Both data are null");
                 return 0;
-            int result;
-            if (o1.getCreatedDate() == o2.getCreatedDate()) {
-                result = 0;
-            } else if (o1.getCreatedDate().isBefore(o2.getCreatedDate())) {
-                result = 1;
-            } else {
-                result = -1;
+
+            } else if (o1.getCreatedDate() == null) {
+
+                logger.info("First date is null");
+                return 1;
+
+            } else if (o2.getCreatedDate() == null) {
+
+                logger.info("Second date is null");
+                return -1;
             }
-            return -1 * result;
+
+            return -1 * o1.getCreatedDate().compareTo(o2.getCreatedDate());
 
         }).collect(Collectors.toList());
     }
