2c9124e035b0abcad37584a13ca384129bec0790
iulianab <iuliana.bratucu.97@gmail.com>
2019-09-04 13:18:22 +0300

    remove unused imports

--- src/main/java/com/cegeka/academy/service/challenge/ChallengeServiceImp.java
+++ src/main/java/com/cegeka/academy/service/challenge/ChallengeServiceImp.java
@@ -18,10 +18,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneId;
 import java.util.*;
 
 import java.util.LinkedHashSet;
